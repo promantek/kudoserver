7d2318b1d0413cbfb379f2f19e8ed65120fcf81e
Shawn Covell <shawn@trakstar.com>
2017-09-15 14:22:07 -0700

    Annotate

--- spec/factories/kudos.rb
+++ spec/factories/kudos.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: kudos
+#
+#  id          :integer          not null, primary key
+#  giver_id    :integer
+#  receiver_id :integer
+#  text        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 FactoryGirl.define do 
   factory :kudo do 
     text { Faker::HitchhikersGuideToTheGalaxy.quote[0..139] }
