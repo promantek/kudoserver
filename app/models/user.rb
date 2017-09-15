af1cd6b36f46b5acef73361634d27bce6155ecd7
Shawn Covell <shawn@trakstar.com>
2017-09-15 14:20:45 -0700

    Add first name / last name fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,13 @@
 #  kudos_received_count :integer
 #  created_at           :datetime         not null
 #  updated_at           :datetime         not null
+#  first_name           :text
+#  last_name            :text
 #
 
 class User < ApplicationRecord
   has_many :kudos_given, class_name: 'Kudo', foreign_key: :giver_id, dependent: :destroy
   has_many :kudos_received, class_name: 'Kudo', foreign_key: :receiver_id
 
-  validates_presence_of :username
+  validates_presence_of :username, :first_name, :last_name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906181227) do
+ActiveRecord::Schema.define(version: 20170914215130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20170906181227) do
     t.integer "kudos_received_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "first_name"
+    t.text "last_name"
   end
 
 end
