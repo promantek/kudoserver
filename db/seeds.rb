d8500a5e8fc73306b75ac783b596ca1b377a17ad
Scott Duncombe <sduncombe@renewfinancial.com>
2017-10-18 10:52:56 -0700

    Clean up org

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,17 @@
 require 'faker'
 
-org = Organization.create!(name: Faker::VentureBros.organization)
+@org = Organization.create!(name: Faker::VentureBros.organization)
 
 def generate_user_data
-  { organization: org,
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  {
+    organization: @org,
+    first_name: first_name,
+    last_name: last_name,
     username: Faker::Internet.user_name("#{first_name} #{last_name}", '_'),
-    password: 'fakefakefake' }
+    password: 'fakefakefake'
+  }
 end
 
 def limited_text(sentence)
--- spec/models/oranization_spec.rb
+++ spec/models/oranization_spec.rb
@@ -1,17 +1,3 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id                   :integer          not null, primary key
-#  username             :string
-#  kudos_given_count    :integer
-#  kudos_received_count :integer
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
-#  first_name           :text
-#  last_name            :text
-#
-
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
